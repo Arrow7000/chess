bd17e7342ff061c554baa55eb6de65dc5bb9837c
Aron Adler <aronadler@hotmail.com>
2017-05-25 16:36:59 +0100

    TS groundwork

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.jsx',
+    entry: './src/index.tsx',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [{
-            test: /.jsx?$/,
-            include: [path.resolve(__dirname, 'src')],
-            exclude: [path.resolve(__dirname, 'node_modules')],
-            loader: 'babel-loader',
-            query: {
-                presets: [
-                    'es2015',
-                    'react'
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /.jsx?$/,
+                include: [path.resolve(__dirname, 'src')],
+                exclude: [path.resolve(__dirname, 'node_modules')],
+                loader: 'babel-loader',
+                query: {
+                    presets: [
+                        'es2015',
+                        'react'
+                    ],
+                    plugins: ['transform-object-rest-spread']
+                }
+            }, {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
                 ],
-                plugins: ['transform-object-rest-spread']
             }
-        }, {
-            test: /\.css$/,
-            use: [
-                { loader: "style-loader" },
-                { loader: "css-loader" },
-            ],
-        }]
+        ]
     },
     resolve: {
-        extensions: ['.json', '.js', '.jsx', '.css']
+        extensions: ['.json', '.js', '.jsx', '.css', '.ts', '.tsx']
     },
     devServer: {
         contentBase: path.join(__dirname, "."),
